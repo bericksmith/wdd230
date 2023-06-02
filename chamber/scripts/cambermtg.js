bb2de2eb984afe5590033b98a229892780a25187
bericksmith <smi23001@byui.edu>
2023-06-02 07:39:50 -0700

    merged js files

--- chamber/scripts/cambermtg.js
+++
@@ -1,9 +0,0 @@
-
-const today = new Date().getDay();
-
-
-const element = document.getElementById("chambermeet");
-
-if (today === 1 || today === 2) {
-    element.style.display = "block";
-}
--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,3 +1,5 @@
+
+// Copyright and date modified
 try {
   let options = {
     year: "numeric",
@@ -27,6 +29,8 @@ try {
   alert("Error with today's date.");
 }
 
+
+// Hamburger Menu
 function toggleMenu() {
   document.getElementById("primaryNav").classList.toggle("open");
   document.getElementById("hamburgerBtn").classList.toggle("open");
@@ -36,4 +40,52 @@ const x = document.getElementById("hamburgerBtn");
 
 x.onclick = toggleMenu;
 
+
+
+// Chamber meeting messages display Monday and Tuesday
+const today = new Date().getDay();
+
+const element = document.getElementById("chambermeet");
+
+if (today === 1 || today === 2) {
+    element.style.display = "block";
+}
+
+
+
+
+// Lazyload
+const allImages = document.querySelectorAll("img[data-src]")
+
+const lazyLoad = (img) => {
+    img.setAttribute("src", img.getAttribute("data-src"))
+    img.onload = () => {
+        img.removeAttribute("data-src")
+        img.className = "ph"
+    };
+};
+
+const options = {
+    threshold: 0,
+    rootMargin: "0px 0px -100px 0px"
+}
+
+if ('IntersectionObserver' in window) {
+    const obsrvr = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if(item.isIntersecting) {
+                lazyLoad(item.target)
+                observer.unobserve(item.target)
+            }
+        })
+    }, options)
+    allImages.forEach((img) => {
+        obsrvr.observe(img)
+    })
+}
+else {
+    allImages.forEach((img) => {
+        lazyLoad(img)
+    })
+}
 
