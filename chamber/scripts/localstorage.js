6a9cf9c2d12e69d3643b6cde925dc77294a00500
bericksmith <smi23001@byui.edu>
2023-06-05 16:53:49 -0700

    w08

--- chamber/scripts/localstorage.js
+++ chamber/scripts/localstorage.js
@@ -1,13 +1,12 @@
-const lastvisitElement = document.getElementById("last-visit");
-const msForDay = 1000 * 60 * 60 * 24;
+  const lastvisitElement = document.getElementById("last-visit")
+  const msInDay = 1000 * 60 * 60 * 24;
 
-let lastVisitTime = Number(window.localStorage.getItem("visittime-ls"));
-let now = new Date(); // Declare and assign the 'now' variable
+  let lastVisitTime = Number(window.localStorage.getItem("visittime-ls"));
 
-if (lastVisitTime !== 0) {
-  lastvisitElement.textContent = Math.round((now.getTime() - lastVisitTime) / msForDay);
-} else {
-  lastvisitElement.textContent = `Welcome! This is your first visit!`;
-}
+  if (lastVisitTime !== 0) {
+      lastvisitElement.textContent = Math.round((now.getTime() - lastVisitTime)/msInDay);
+  } else {
+    lastvisitElement.textContent = `Welcome!! This is your first time here!`;
+  }
 
-localStorage.setItem("visittime-ls", now.getTime());
+  localStorage.setItem("visittime-ls", now.getTime());
