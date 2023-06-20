795db0aa0ada3101aee73674050cf6b9d98bc24b
bericksmith <smi23001@byui.edu>
2023-06-20 06:44:10 -0700

    weather api as html

--- lesson10/scripts/weatherapi.js
+++ lesson10/scripts/weatherapi.js
@@ -2,10 +2,9 @@ const apiKey = 'bb06f83ae63888ef53975084692c8460';
 const url = `https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=${apiKey}`;
 
 // select HTML elements in the document
-const currentTemp = document.querySelector("#current-temp");
-const weatherIcon = document.querySelector("#weather-icon");
-const captionDesc = document.querySelector("figcaption");
-
+//const currentTemp = document.querySelector("#current-temp");
+//const weatherIcon = document.querySelector("#weather-icon");
+//const captionDesc = document.querySelector("figcaption");
 
 async function apiFetch() {
   try {
@@ -23,14 +22,31 @@ async function apiFetch() {
 }
 
 function displayResults(weatherData) {
-  currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+  const weatherInfoElement = document.getElementById('weather-info');
+  const temp = weatherData.main.temp.toFixed(0);
   const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
   const desc = weatherData.weather[0].description;
+  //currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+  //weatherIcon.setAttribute("src", iconsrc);
+  // weatherIcon.setAttribute("alt", desc);
+  //captionDesc.textContent = desc;
 
-  weatherIcon.setAttribute("src", iconsrc);
-  weatherIcon.setAttribute("alt", desc);
-  captionDesc.textContent = desc;
+  const htmlContent = `
+    <p>The current temperature in Fairbanks, Alaska is <strong>${temp}°F</strong></p>
+    <h2>Current Condition</h2>
+    <figure>
+      <img class="weather-img" alt="Weather icon" src="${iconsrc}">
+      <img src="" alt="" id="weather-icon">
+      <figcaption>${desc}</figcaption>
+    </figure>
+  `;
+
+  weatherInfoElement.innerHTML = htmlContent;
+
+
+
 }
 
 apiFetch();
+
 
