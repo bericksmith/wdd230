000a86b3e6e5f9c016d3d2062e72bd210fdf1f60
bericksmith <smi23001@byui.edu>
2023-06-30 09:06:01 -0700

    bountiful final

---
+++ bountiful/scripts/customdrink.js
@@ -0,0 +1,31 @@
+
+    if (typeof(Storage) !== "undefined") {
+        localStorage.setItem("customDrink", 6);
+      
+        const customDrink = localStorage.getItem("customDrink");
+              
+        if (customDrink > 0) {
+            document.getElementById("drinkInfo").innerHTML = `
+            <form action="fresh.html">
+            <button class="customdrinkbtn" type="submit">
+            You have created ${customDrink} Custom Drinks! Create another?
+            </button>
+            </form>
+            `;
+
+
+
+              
+
+
+        } else {
+            document.getElementById("drinkInfo").innerHTML = "<p>Create your first custom drink!</p>";
+        }
+        
+        } else {
+ 
+        document.getElementById("drinkInfo").innerHTML = "<p>Web storage is not supported in this browser.</p>";
+        }
+
+
+          
